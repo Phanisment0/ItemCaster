b4c9643b77f1f0b6b03fa511488ad076e0027f26
Agil <phanisment0@gmail.com>
2025-10-07 03:18:48 +0000

    Code Abilities Menu

--- src/main/java/io/phanisment/itemcaster/menu/editor/AbilitiesMenu.java
+++ src/main/java/io/phanisment/itemcaster/menu/editor/AbilitiesMenu.java
@@ -1,10 +1,17 @@
 package io.phanisment.itemcaster.menu.editor;
 
 import fr.mrmicky.fastinv.PaginatedFastInv;
+import io.phanisment.itemcaster.item.CasterItem;
 import fr.mrmicky.fastinv.ItemBuilder;
+
+import java.util.Map;
+import java.util.List;
+
+import org.bukkit.Material;
+
 import fr.mrmicky.fastinv.InventoryScheme;
 
-public class AbilitiesButton extends PaginatedFastInv {
+public class AbilitiesMenu extends PaginatedFastInv {
 	private InventoryScheme scheme = new InventoryScheme()
 		.mask("111111111")
 		.mask("111111111")
@@ -13,7 +20,18 @@ public class AbilitiesButton extends PaginatedFastInv {
 		.mask("111111111")
 		.bindPagination('1');
 	
-	public AbilitiesButton() {
+	public AbilitiesMenu(CasterItem item) {
+		super(54, "Item Abilities");
+		nextPageItem(54, new ItemBuilder(Material.ARROW).name("Next Page").build());
+		previousPageItem(53, new ItemBuilder(Material.ARROW).name("Previous Page").build());
+		List<Map<String, Object>> abilities = item.getAbilities();
+		int index_ability = 0;
+		for (Map<String, Object> ability : abilities) {
+			index_ability++;
+			addContent(new ItemBuilder(Material.GRAY_DYE).name("Ability Slot: " + index_ability).build(), e -> {
+
+			});
+		}
 		scheme.apply(this);
 	}
 }
